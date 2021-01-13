3c3198a101d88a99360fbad0b0437483c405cb10
wuhao103 <wuhao103@fiberhome.com>
2021-01-13 23:23:20 +0800

    feat: 文本居中

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,9 @@ class ZXYHomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       "早发白帝城 \n[唐] 李白\n 朝辞白帝彩云间，千里江陵一日还。\n 两岸猿声啼不住，轻舟已过万重山",
+      textAlign: TextAlign.center,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
       style: TextStyle(
         fontSize: 20,
         color: Colors.purple,
