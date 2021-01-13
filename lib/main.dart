e8a01ca38fad097988649a193cbfe401e387b877
wuhao103 <wuhao103@fiberhome.com>
2021-01-13 23:14:09 +0800

    feat: user Container增加边框

--- lib/main.dart
+++ lib/main.dart
@@ -6,52 +6,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ZXYHomePage(),
+      home: HYHomePage(),
     );
   }
 }
 
-class ZXYHomePage extends StatelessWidget {
+class HYHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("商品列表"),
       ),
-      body: ZXYHomeContent(),
+      body: HYHomeContent(),
     );
   }
 }
-class ZXYHomeContent extends StatelessWidget {
+
+class HYHomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        ZXYHomeProductItem("Apple1", "Macbook Product1", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg"),
-        ZXYHomeProductItem("Apple2", "Macbook Product2", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg"),
-        ZXYHomeProductItem("Apple3", "Macbook Product3", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg"),
+        HYHomeProductItem("Apple1", "Macbook1", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg"),
+        SizedBox(height: 6,),
+        HYHomeProductItem("Apple2", "Macbook2", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg"),
+        SizedBox(height: 6,),
+        HYHomeProductItem("Apple3", "Macbook2", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg"),
       ],
     );
   }
 }
 
-
-class ZXYHomeProductItem extends StatelessWidget {
-
+class HYHomeProductItem extends StatelessWidget {
   final String title;
   final String desc;
   final String imageURL;
 
-  ZXYHomeProductItem(this.title, this.desc, this.imageURL);
+  final style1 = TextStyle(fontSize: 25, color: Colors.orange);
+  final style2 = TextStyle(fontSize: 20, color: Colors.green);
+
+  HYHomeProductItem(this.title, this.desc, this.imageURL);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(title, style: TextStyle(fontSize: 24)),
-        Text(desc, style: TextStyle(fontSize: 18)),
-        Image.network(imageURL)
-      ],
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          border: Border.all(
+              width: 5, // 设置边框的宽度
+              color: Colors.black// 设置边框的颜色
+          )
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(title, style: style1),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text(desc, style: style2),
+          SizedBox(height: 8),
+          Image.network(imageURL)
+        ],
+      ),
     );
   }
 }
