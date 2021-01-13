e63fafb63127007c8b8ad27fb73031ae26e869c5
wuhao103 <wuhao103@fiberhome.com>
2021-01-13 22:56:43 +0800

    use meterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Hello World",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.orange,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('title'),
         ),
-      ),
-    );
+        body: Center(
+          child: Text(
+            'data',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.blue, fontSize: 30),
+          ),
+        ),
+    ));
   }
 }
