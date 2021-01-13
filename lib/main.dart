8b3feae25ff378fce48bf90b6165679114e56cc6
wuhao103 <wuhao103@fiberhome.com>
2021-01-13 23:05:02 +0800

    refactor 使用静态组件stateLessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//使用箭头函数，当只有一个表达体时
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('title'),
-            ),
-            body: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Checkbox(
-                      value: true, onChanged: (value) => print("Hello World")),
-                  Text(
-                    "同意协议",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(fontSize: 20),
-                  )
-                ],
-              ),
-            )));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("第一个Flutter项目"),
+        ),
+        body: HomeContent(),
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Checkbox(
+              value: true,
+              onChanged: (value) => print("Hello World")),
+          Text(
+            "同意协议",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 20),
+          )
+        ],
+      ),
+    );
   }
 }
